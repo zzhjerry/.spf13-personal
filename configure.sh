6ba14d6722b99b019228e36d8862c4766a4a4c73
zzh699 <zzh699@gmail.com>
2016-11-05 21:39:39 +0800

    Add personal configurations

---
+++ configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s  ~/.spf13-personal/.vimrc.before.fork    ~/.vimrc.before.fork
+ln -s  ~/.spf13-personal/.vimrc.before.local   ~/.vimrc.before.local
+ln -s  ~/.spf13-personal/.vimrc.bundles.fork   ~/.vimrc.bundles.fork
+ln -s  ~/.spf13-personal/.vimrc.bundles.local  ~/.vimrc.bundles.local
+ln -s  ~/.spf13-personal/.vimrc.fork           ~/.vimrc.fork
+ln -s  ~/.spf13-personal/.vimrc.local          ~/.vimrc.local
